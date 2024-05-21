f218bea4f4350b9f1299f6b0be80f6e549655dc6
kotanys <rudometov.vadim123@gmail.com>
2024-05-21 22:58:33 +0300

    little reordering

--- main.py
+++ main.py
@@ -62,14 +62,14 @@ def main(event_handler: None | Callable[[Event], None] = None):
             total += 1
 
             if valid:
-                print(' ВЕРНО')
+                event_handler('correct')
                 score += 1
                 errors.discard(test_word)
-                event_handler('correct')
+                print(' ВЕРНО')
             else:
                 event_handler('wrong')
-                print(f' НЕВЕРНО. {test_word}')
                 errors.add(test_word)
+                print(f' НЕВЕРНО. {test_word}')
             print(flush=True)
 
             last_pressed = None
